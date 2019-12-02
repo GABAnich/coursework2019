a133f005d3bea15b51afd0f0733190a08a74bcc0
GABAnich <doofixxx@gmail.com>
2019-12-03 01:51:47 +0200

    Added mockup

--- qt/abstract-factory/mainwindow.cpp
+++ qt/abstract-factory/mainwindow.cpp
@@ -1,16 +1,52 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QPixmap>
+#include <QStandardItemModel>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 
-    QPalette pal = palette();
-    pal.setColor(QPalette::Background, QColor(233, 30, 99));
-    this->setAutoFillBackground(true);
-    this->setPalette(pal);
+    ui->tableView->verticalHeader()->setDefaultSectionSize(170);
+    ui->tableView->horizontalHeader()->setDefaultSectionSize(200);
+
+    QImage mc("/home/gaba/Desktop/coursework2019/images/mc.png");
+    QImage ms("/home/gaba/Desktop/coursework2019/images/ms.png");
+    QImage mct("/home/gaba/Desktop/coursework2019/images/mct.png");
+    QImage vc("/home/gaba/Desktop/coursework2019/images/vc.png");
+    QImage vs("/home/gaba/Desktop/coursework2019/images/vs.png");
+    QImage vct("/home/gaba/Desktop/coursework2019/images/vct.png");
+
+    QStandardItemModel *model = new QStandardItemModel();
+
+    QStandardItem *item00 = new QStandardItem();
+    item00->setData(QVariant(QPixmap::fromImage(mc)), Qt::DecorationRole);
+    model->setItem(0, 0, item00);
+
+    QStandardItem *item01 = new QStandardItem();
+    item01->setData(QVariant(QPixmap::fromImage(ms)), Qt::DecorationRole);
+    model->setItem(0, 1, item01);
+
+    QStandardItem *item02 = new QStandardItem();
+    item02->setData(QVariant(QPixmap::fromImage(vs)), Qt::DecorationRole);
+    model->setItem(0, 2, item02);
+
+    QStandardItem *item10 = new QStandardItem();
+    item10->setData(QVariant(QPixmap::fromImage(vc)), Qt::DecorationRole);
+    model->setItem(1, 0, item10);
+
+    QStandardItem *item11 = new QStandardItem();
+    item11->setData(QVariant(QPixmap::fromImage(mct)), Qt::DecorationRole);
+    model->setItem(1, 1, item11);
+
+    QStandardItem *item12 = new QStandardItem();
+    item12->setData(QVariant(QPixmap::fromImage(vct)), Qt::DecorationRole);
+    model->setItem(1, 2, item12);
+
+    ui->tableView->setModel(model);
 }
 
 MainWindow::~MainWindow()
--- qt/build-abstract-factory-Desktop_Qt_5_12_2_GCC_64bit-Debug/ui_mainwindow.h
+++ qt/build-abstract-factory-Desktop_Qt_5_12_2_GCC_64bit-Debug/ui_mainwindow.h
@@ -11,8 +11,11 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QComboBox>
 #include <QtWidgets/QHeaderView>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QTableView>
 #include <QtWidgets/QWidget>
 
@@ -23,6 +26,13 @@ class Ui_MainWindow
 public:
     QWidget *centralwidget;
     QTableView *tableView;
+    QLabel *label;
+    QLabel *label_2;
+    QComboBox *comboBox;
+    QPushButton *pushButton;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton_3;
+    QPushButton *pushButton_4;
 
     void setupUi(QMainWindow *MainWindow)
     {
@@ -30,11 +40,61 @@ public:
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
         MainWindow->resize(960, 600);
         MainWindow->setAutoFillBackground(true);
+        MainWindow->setStyleSheet(QString::fromUtf8("MainWindow {\n"
+"	background-color: #228c22;\n"
+"}"));
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         tableView = new QTableView(centralwidget);
         tableView->setObjectName(QString::fromUtf8("tableView"));
-        tableView->setGeometry(QRect(360, 0, 600, 600));
+        tableView->setGeometry(QRect(309, 0, 651, 600));
+        tableView->setStyleSheet(QString::fromUtf8(""));
+        label = new QLabel(centralwidget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(10, 10, 291, 51));
+        QFont font;
+        font.setFamily(QString::fromUtf8("Fantasque Sans Mono"));
+        font.setPointSize(26);
+        font.setBold(true);
+        font.setWeight(75);
+        label->setFont(font);
+        label_2 = new QLabel(centralwidget);
+        label_2->setObjectName(QString::fromUtf8("label_2"));
+        label_2->setGeometry(QRect(10, 130, 291, 31));
+        QFont font1;
+        font1.setFamily(QString::fromUtf8("Fantasque Sans Mono"));
+        font1.setPointSize(18);
+        label_2->setFont(font1);
+        comboBox = new QComboBox(centralwidget);
+        comboBox->addItem(QString());
+        comboBox->addItem(QString());
+        comboBox->setObjectName(QString::fromUtf8("comboBox"));
+        comboBox->setGeometry(QRect(10, 170, 291, 25));
+        QFont font2;
+        font2.setFamily(QString::fromUtf8("Fantasque Sans Mono"));
+        font2.setPointSize(16);
+        comboBox->setFont(font2);
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(10, 300, 291, 25));
+        pushButton->setFont(font2);
+        pushButton_2 = new QPushButton(centralwidget);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setGeometry(QRect(10, 340, 291, 25));
+        pushButton_2->setFont(font2);
+        pushButton_3 = new QPushButton(centralwidget);
+        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
+        pushButton_3->setGeometry(QRect(10, 380, 291, 25));
+        pushButton_3->setFont(font2);
+        pushButton_4 = new QPushButton(centralwidget);
+        pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
+        pushButton_4->setGeometry(QRect(10, 530, 291, 61));
+        QFont font3;
+        font3.setFamily(QString::fromUtf8("Fantasque Sans Mono"));
+        font3.setPointSize(32);
+        font3.setBold(true);
+        font3.setWeight(75);
+        pushButton_4->setFont(font3);
         MainWindow->setCentralWidget(centralwidget);
 
         retranslateUi(MainWindow);
@@ -45,6 +105,15 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        label->setText(QApplication::translate("MainWindow", "Abstract Factory", nullptr));
+        label_2->setText(QApplication::translate("MainWindow", "Select a factory:", nullptr));
+        comboBox->setItemText(0, QApplication::translate("MainWindow", "Modern Furniture Factory", nullptr));
+        comboBox->setItemText(1, QApplication::translate("MainWindow", "Victorian Furniture Factory", nullptr));
+
+        pushButton->setText(QApplication::translate("MainWindow", "Create a chair", nullptr));
+        pushButton_2->setText(QApplication::translate("MainWindow", "Create a sofa", nullptr));
+        pushButton_3->setText(QApplication::translate("MainWindow", "Create a coffee table", nullptr));
+        pushButton_4->setText(QApplication::translate("MainWindow", "PDF", nullptr));
     } // retranslateUi
 
 };
